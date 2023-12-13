f4690dfedd4fcce8b35d131441228dfe31adc5a6
Bezzagame <102792810+Bezzagame@users.noreply.github.com>
2023-12-13 19:25:11 +0700

    added main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from string import ascii_letters
+
+ALPHABET = list(ascii_letters)
+
+message = input('Введите сообщение:')
+key = input('Введите приватный код:')
+
+key_message = (key * 10)[:len(message)]
+encrypted_message = ''
+
+for i in range(len(message)):
+    letter_index_message = ALPHABET.index(message(i))
+    letter_index_key = ALPHABET.index(key_message(i))
+    letter_index = letter_index_message + letter_index_key
+
+    if letter_index > len(ALPHABET):
+        letter_index -= len(ALPHABET)
